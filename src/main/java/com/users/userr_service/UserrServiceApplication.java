6582077c28ea6422c00bee03b12309a0b3cb3831
mateeullahkhakhilji <mateeullahkhilji.sbmsales@gmail.com>
2025-12-25 19:35:59 +0500

    first code

---
+++ src/main/java/com/users/userr_service/UserrServiceApplication.java
@@ -0,0 +1,13 @@
+package com.users.userr_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserrServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserrServiceApplication.class, args);
+    }
+
+}
